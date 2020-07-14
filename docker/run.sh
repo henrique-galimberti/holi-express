96303f0ae3acdde6a29dd3afe81476af3cd78b1b
lhg31 <henriquegalimberti@gmail.com>
2020-07-14 17:06:01 -0300

    fixed postgres service name

--- docker/run.sh
+++ docker/run.sh
@@ -6,7 +6,7 @@ export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker-compose -f $DIR/docker-compose.yml stop
 
-docker-compose -f $DIR/docker-compose.yml up -d redis postgresql
+docker-compose -f $DIR/docker-compose.yml up -d redis postgres
 echo "Waiting for databases to init..."
 sleep 30
 
