55388059e760806bace8acf0b3afdb931880d23f
lhg31 <henriquegalimberti@gmail.com>
2020-07-15 20:18:28 -0300

    added unit tests

--- stock-microservice/src/test/java/com/ilegra/holiexpress/stock/StockVerticleTest.java
+++ stock-microservice/src/test/java/com/ilegra/holiexpress/stock/StockVerticleTest.java
@@ -1,69 +1,69 @@
-//package com.ilegra.holiexpress.stock;
-//
-//import io.vertx.core.DeploymentOptions;
-//import io.vertx.core.Vertx;
-//import io.vertx.core.json.JsonObject;
-//import io.vertx.ext.jdbc.JDBCClient;
-//import io.vertx.ext.unit.Async;
-//import io.vertx.ext.unit.TestContext;
-//import io.vertx.ext.unit.junit.VertxUnitRunner;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.powermock.core.classloader.annotations.PrepareForTest;
-//import org.powermock.modules.junit4.PowerMockRunner;
-//import org.powermock.modules.junit4.PowerMockRunnerDelegate;
-//import redis.embedded.RedisServer;
-//
-//import java.io.IOException;
-//import java.net.ServerSocket;
-//
-//@RunWith(PowerMockRunner.class)
-//@PowerMockRunnerDelegate(VertxUnitRunner.class)
-//@PrepareForTest({JDBCClient.class})
-//public class StockVerticleTest {
-//
-//    private RedisServer server;
-//    private String deploymentId;
-//    private int port;
-//
-//    @Before
-//    public void mockRedis() throws IOException {
-//        ServerSocket serverSocket = new ServerSocket(0);
-//        port = serverSocket.getLocalPort();
-//        serverSocket.close();
-//        server = new RedisServer(port);
-//        server.start();
-//    }
-//
-//    @Test
-//    public void start_stop(TestContext testContext) {
-//        Vertx vertx = Vertx.vertx();
-//
-//        StockVerticle verticle = new StockVerticle();
-//
-//        Async asyncStart = testContext.async();
-//        vertx.deployVerticle(verticle, new DeploymentOptions()
-//                        .setConfig(new JsonObject()
-//                                .put("redis.connection", "redis://localhost:" + port)),
-//                asyncResult -> {
-//                    testContext.assertTrue(asyncResult.succeeded());
-//                    deploymentId = asyncResult.result();
-//                    asyncStart.complete();
-//                });
-//        asyncStart.await(5000);
-//
-//        Async asyncStop = testContext.async();
-//        vertx.undeploy(deploymentId, asyncResult -> {
-//            testContext.assertTrue(asyncResult.succeeded());
-//            asyncStop.complete();
-//        });
-//        asyncStop.await(5000);
-//    }
-//
-//    @After
-//    public void stopRedis() {
-//        server.stop();
-//    }
-//}
+package com.ilegra.holiexpress.stock;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(VertxUnitRunner.class)
+@PrepareForTest({JDBCClient.class})
+public class StockVerticleTest {
+
+    private RedisServer server;
+    private String deploymentId;
+    private int port;
+
+    @Before
+    public void mockRedis() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(0);
+        port = serverSocket.getLocalPort();
+        serverSocket.close();
+        server = new RedisServer(port);
+        server.start();
+    }
+
+    @Test
+    public void start_stop(TestContext testContext) {
+        Vertx vertx = Vertx.vertx();
+
+        StockVerticle verticle = new StockVerticle();
+
+        Async asyncStart = testContext.async();
+        vertx.deployVerticle(verticle, new DeploymentOptions()
+                        .setConfig(new JsonObject()
+                                .put("redis.connection", "redis://localhost:" + port)),
+                asyncResult -> {
+                    testContext.assertTrue(asyncResult.succeeded());
+                    deploymentId = asyncResult.result();
+                    asyncStart.complete();
+                });
+        asyncStart.await(5000);
+
+        Async asyncStop = testContext.async();
+        vertx.undeploy(deploymentId, asyncResult -> {
+            testContext.assertTrue(asyncResult.succeeded());
+            asyncStop.complete();
+        });
+        asyncStop.await(5000);
+    }
+
+    @After
+    public void stopRedis() {
+        server.stop();
+    }
+}
--- stock-microservice/src/test/java/com/ilegra/holiexpress/stock/api/StockRestAPIVerticleTest.java
+++ stock-microservice/src/test/java/com/ilegra/holiexpress/stock/api/StockRestAPIVerticleTest.java
@@ -1,65 +1,65 @@
-//package com.ilegra.holiexpress.stock.api;
-//
-//import io.vertx.core.DeploymentOptions;
-//import io.vertx.core.Vertx;
-//import io.vertx.core.json.JsonObject;
-//import io.vertx.ext.unit.Async;
-//import io.vertx.ext.unit.TestContext;
-//import io.vertx.ext.unit.junit.VertxUnitRunner;
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.powermock.modules.junit4.PowerMockRunner;
-//import org.powermock.modules.junit4.PowerMockRunnerDelegate;
-//import redis.embedded.RedisServer;
-//
-//import java.io.IOException;
-//import java.net.ServerSocket;
-//
-//@RunWith(PowerMockRunner.class)
-//@PowerMockRunnerDelegate(VertxUnitRunner.class)
-//public class StockRestAPIVerticleTest {
-//
-//    private RedisServer server;
-//    private String deploymentId;
-//    private int port;
-//
-//    @Before
-//    public void mockRedis() throws IOException {
-//        ServerSocket serverSocket = new ServerSocket(0);
-//        port = serverSocket.getLocalPort();
-//        serverSocket.close();
-//        server = new RedisServer(port);
-//        server.start();
-//    }
-//
-//    @Test
-//    public void start_stop(TestContext testContext) {
-//        Vertx vertx = Vertx.vertx();
-//
-//        StockRestAPIVerticle verticle = new StockRestAPIVerticle(null);
-//
-//        Async asyncStart = testContext.async();
-//        vertx.deployVerticle(verticle, new DeploymentOptions()
-//                .setConfig(new JsonObject()
-//                        .put("redis.connection", "redis://localhost:" + port)), asyncResult -> {
-//            testContext.assertTrue(asyncResult.succeeded());
-//            deploymentId = asyncResult.result();
-//            asyncStart.complete();
-//        });
-//        asyncStart.await(5000);
-//
-//        Async asyncStop = testContext.async();
-//        vertx.undeploy(deploymentId, asyncResult -> {
-//            testContext.assertTrue(asyncResult.succeeded());
-//            asyncStop.complete();
-//        });
-//        asyncStop.await(5000);
-//    }
-//
-//    @After
-//    public void stopRedis() {
-//        server.stop();
-//    }
-//}
+package com.ilegra.holiexpress.stock.api;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(VertxUnitRunner.class)
+public class StockRestAPIVerticleTest {
+
+    private RedisServer server;
+    private String deploymentId;
+    private int port;
+
+    @Before
+    public void mockRedis() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(0);
+        port = serverSocket.getLocalPort();
+        serverSocket.close();
+        server = new RedisServer(port);
+        server.start();
+    }
+
+    @Test
+    public void start_stop(TestContext testContext) {
+        Vertx vertx = Vertx.vertx();
+
+        StockRestAPIVerticle verticle = new StockRestAPIVerticle(null);
+
+        Async asyncStart = testContext.async();
+        vertx.deployVerticle(verticle, new DeploymentOptions()
+                .setConfig(new JsonObject()
+                        .put("redis.connection", "redis://localhost:" + port)), asyncResult -> {
+            testContext.assertTrue(asyncResult.succeeded());
+            deploymentId = asyncResult.result();
+            asyncStart.complete();
+        });
+        asyncStart.await(5000);
+
+        Async asyncStop = testContext.async();
+        vertx.undeploy(deploymentId, asyncResult -> {
+            testContext.assertTrue(asyncResult.succeeded());
+            asyncStop.complete();
+        });
+        asyncStop.await(5000);
+    }
+
+    @After
+    public void stopRedis() {
+        server.stop();
+    }
+}
