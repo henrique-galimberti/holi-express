9a12559bf52e8997b5dfb466b41c08f955b3366a
lhg31 <henriquegalimberti@gmail.com>
2020-07-16 12:42:54 -0300

    better logs for stock service

--- stock-microservice/src/main/java/com/ilegra/holiexpress/stock/service/StockServiceImpl.java
+++ stock-microservice/src/main/java/com/ilegra/holiexpress/stock/service/StockServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ilegra.holiexpress.stock.service;
 
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
 import io.vertx.redis.client.*;
 
 public class StockServiceImpl implements StockService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(StockServiceImpl.class);
 
     private static final String PREFIX = "stock:";
 
@@ -17,26 +22,42 @@ public class StockServiceImpl implements StockService {
         RedisOptions redisOptions = new RedisOptions()
                 .setConnectionString(config.getString("redis.connection", "redis://localhost:6379"));
         Redis.createClient(vertx, redisOptions).connect(onConnect -> {
-            conn = onConnect.result();
-            redis = RedisAPI.api(conn);
+            if (onConnect.succeeded()) {
+                conn = onConnect.result();
+                redis = RedisAPI.api(conn);
+            } else {
+                LOGGER.fatal("Failed to connect to redis.", onConnect.cause());
+            }
         });
     }
 
     public void increaseStock(String productId, String amount, Handler<AsyncResult<Integer>> resultHandler) {
         redis.incrby(PREFIX + productId, amount, asyncResult -> {
-            resultHandler.handle(asyncResult.map(Response::toInteger));
+            if (asyncResult.succeeded()) {
+                resultHandler.handle(asyncResult.map(Response::toInteger));
+            } else {
+                resultHandler.handle(Future.failedFuture(asyncResult.cause()));
+            }
         });
     }
 
     public void decreaseStock(String productId, String amount, Handler<AsyncResult<Integer>> resultHandler) {
         redis.decrby(PREFIX + productId, amount, asyncResult -> {
-            resultHandler.handle(asyncResult.map(Response::toInteger));
+            if (asyncResult.succeeded()) {
+                resultHandler.handle(asyncResult.map(Response::toInteger));
+            } else {
+                resultHandler.handle(Future.failedFuture(asyncResult.cause()));
+            }
         });
     }
 
     public void retrieveStock(String productId, Handler<AsyncResult<Integer>> resultHandler) {
         redis.get(PREFIX + productId, asyncResult -> {
-            resultHandler.handle(asyncResult.map(Response::toInteger));
+            if (asyncResult.succeeded()) {
+                resultHandler.handle(asyncResult.map(Response::toInteger));
+            } else {
+                resultHandler.handle(Future.failedFuture(asyncResult.cause()));
+            }
         });
     }
 }
